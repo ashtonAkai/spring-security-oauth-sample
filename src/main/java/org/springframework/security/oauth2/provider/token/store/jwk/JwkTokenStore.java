5623cc0447d34341d187c3d098bd958cf8cfaa98
Joe Grandja <jgrandja@pivotal.io>
2017-02-03 15:18:17 -0500

    Cleanup exception throwing

--- src/main/java/org/springframework/security/oauth2/provider/token/store/jwk/JwkTokenStore.java
+++ src/main/java/org/springframework/security/oauth2/provider/token/store/jwk/JwkTokenStore.java
@@ -50,7 +50,7 @@ public class JwkTokenStore implements TokenStore {
 
 	@Override
 	public void storeAccessToken(OAuth2AccessToken token, OAuth2Authentication authentication) {
-		this.delegate.storeAccessToken(token, authentication);
+		throw this.operationNotSupported();
 	}
 
 	@Override
@@ -60,46 +60,50 @@ public class JwkTokenStore implements TokenStore {
 
 	@Override
 	public void removeAccessToken(OAuth2AccessToken token) {
-		this.delegate.removeAccessToken(token);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public void storeRefreshToken(OAuth2RefreshToken refreshToken, OAuth2Authentication authentication) {
-		this.delegate.storeRefreshToken(refreshToken, authentication);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public OAuth2RefreshToken readRefreshToken(String tokenValue) {
-		return this.delegate.readRefreshToken(tokenValue);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public OAuth2Authentication readAuthenticationForRefreshToken(OAuth2RefreshToken token) {
-		return this.delegate.readAuthenticationForRefreshToken(token);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public void removeRefreshToken(OAuth2RefreshToken token) {
-		this.delegate.removeRefreshToken(token);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public void removeAccessTokenUsingRefreshToken(OAuth2RefreshToken refreshToken) {
-		this.delegate.removeAccessTokenUsingRefreshToken(refreshToken);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public OAuth2AccessToken getAccessToken(OAuth2Authentication authentication) {
-		return this.delegate.getAccessToken(authentication);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public Collection<OAuth2AccessToken> findTokensByClientIdAndUserName(String clientId, String userName) {
-		return this.delegate.findTokensByClientIdAndUserName(clientId, userName);
+		throw this.operationNotSupported();
 	}
 
 	@Override
 	public Collection<OAuth2AccessToken> findTokensByClientId(String clientId) {
-		return this.delegate.findTokensByClientId(clientId);
+		throw this.operationNotSupported();
+	}
+
+	private JwkException operationNotSupported() {
+		return new JwkException("This operation is currently not supported.");
 	}
 }
